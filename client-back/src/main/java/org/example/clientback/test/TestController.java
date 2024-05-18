50a16f9b3228058a398f9a115ee1a17a5e78f622
KWY <wodrk1001@gmail.com>
2024-05-18 13:48:35 +0900

    [feat] docker compose 구현

---
+++ client-back/src/main/java/org/example/clientback/test/TestController.java
@@ -0,0 +1,12 @@
+package org.example.clientback.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "client server on";
+    }
+}
