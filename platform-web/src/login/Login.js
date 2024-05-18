b12f8afa0308e952b3ae7f3abf1c54ac926058b6
KWY <wodrk1001@gmail.com>
2024-05-19 03:08:01 +0900

    [feat] redirect 해결
    fetch는 2016년 이후로 리다이렉션 시 페이지를 이동하지 않는다...

--- platform-web/src/login/Login.js
+++ platform-web/src/login/Login.js
@@ -1,25 +1,19 @@
 import React from "react"
 
 function Login() {
-    const SUCCESS_URL = "http://localhost:8080/authorize?code=1234&client_id=client_id&redirect_url=http://localhost:3001";
-    const FAIL_URL = "http://localhost:8080/authorize?code=1&client_id=client_id&redirect_url=http://localhost:3001";
-    const getAuthorizeCode = (url) => {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                // 여기서 data를 처리합니다.
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+
+    const successCase = () => {
+        window.location.href = "http://localhost:8080/authorize?code=1234&client_id=client_id&redirect_url=http://localhost:3001";
+    };
+    const failCase = () => {
+        window.location.href = "http://localhost:8080/authorize?code=1&client_id=client_id&redirect_url=http://localhost:3001";
     };
 
     return (
         <div className="login-container">
             <h1>플랫폼 로그인</h1>
-            <button onClick={() => getAuthorizeCode(SUCCESS_URL)}>소셜로그인 성공</button>
-            <button onClick={() => getAuthorizeCode(FAIL_URL)}>소셜로그인 실패</button>
+            <button onClick={() => successCase()}>소셜로그인 성공</button>
+            <button onClick={() => failCase()}>소셜로그인 실패</button>
         </div>
     )
 }
