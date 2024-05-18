4636b5ff25382554c776e9fca57e58cbf00f02d0
KWY <wodrk1001@gmail.com>
2024-05-18 11:02:11 +0900

    [test]

--- platform-back/src/main/java/org/example/platformback/auth/AuthController.java
+++ platform-back/src/main/java/org/example/platformback/auth/AuthController.java
@@ -3,6 +3,7 @@ package org.example.platformback.auth;
 import lombok.RequiredArgsConstructor;
 import org.example.platformback.auth.dto.request.AuthReqDto;
 import org.example.platformback.auth.dto.response.TokenResDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,6 @@ import java.net.URISyntaxException;
 public class AuthController {
     private final AuthService authService;
 
-    // http://localhost:8080/authorize?code=1234&client_id=client_id&redirect_url=http://localhost:3000/login
     @GetMapping("/authorize")
     public ResponseEntity<String> authorizeCode(
             @RequestParam("code") String code,
@@ -27,8 +27,8 @@ public class AuthController {
     ) throws URISyntaxException {
         Long userId = authService.getUserId(code);
         if (userId == null) return ResponseEntity
-                .notFound()
-                .build();
+                .status(HttpStatus.NOT_FOUND)
+                .body("존재하지 않는 유저입니다.");
 
         return ResponseEntity
                 .status(302)
