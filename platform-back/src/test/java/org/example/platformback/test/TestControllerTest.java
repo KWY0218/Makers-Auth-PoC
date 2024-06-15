952b95742f7bb3cfa86475a53edb1c146a9dac28
KWY <wodrk1001@gmail.com>
2024-06-16 01:32:50 +0900

    ci test fix

--- platform-back/src/test/java/org/example/platformback/test/TestControllerTest.java
+++ platform-back/src/test/java/org/example/platformback/test/TestControllerTest.java
@@ -6,15 +6,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = {TestController.class})
-@Import({TestController.class})
+@SpringBootTest
+@AutoConfigureMockMvc
 class TestControllerTest {
     @Autowired
     private MockMvc mockMvc;
