83380f76d2846987fa8d996b7589a3ce2b9fed57
KWY <wodrk1001@gmail.com>
2024-06-16 12:12:32 +0900

    [feat] 검증 로직 추가

--- platform-back/src/main/java/org/example/platformback/test/TestController.java
+++ platform-back/src/main/java/org/example/platformback/test/TestController.java
@@ -1,12 +1,23 @@
 package org.example.platformback.test;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
     @GetMapping("/test")
     public String test() {
         return "platform server on";
+    }
+
+    @GetMapping("/secret")
+    public ResponseEntity<String> secret() {
+        System.out.println("secret key is " + secretKey);
+        return ResponseEntity.ok("secret key is " + secretKey);
     }
 }
